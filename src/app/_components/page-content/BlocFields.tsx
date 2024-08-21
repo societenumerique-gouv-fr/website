f93ae42bb53628bbf0d8eb03d7941639b1cea708
Laura Miguel <laura.miguel.v2.4@gmail.com>
2024-08-21 14:20:33 +0200

    fix: restauring blocfields file

--- src/app/_components/page-content/BlocFields.tsx
+++ src/app/_components/page-content/BlocFields.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import { marginsBottom } from '../structs';
 
+type EspacementBas = 'default' | 'px8' | 'px16' | 'px32' | 'px48' | 'px64';
+
 type BlocFieldSimple = {
   id: string;
   page_cible: string;
@@ -11,7 +13,7 @@ type BlocFieldSimple = {
 };
 
 type DataProps = {
-  espacement_bas: string;
+  espacement_bas: EspacementBas;
   titre: string;
   Bloc_simple: BlocFieldSimple[];
 };
@@ -21,28 +23,28 @@ type BlocFieldsProps = {
 };
 
 export const BlocFields = ({ data }: BlocFieldsProps) => {
+  const marginBottomValue = marginsBottom[data.espacement_bas] || '0px';
+
   return (
     <div className='flexrow-container'>
-      <div className='champ-de-blocs' style={{ marginBottom: marginsBottom[data.espacement_bas] }}>
+      <div className='champ-de-blocs' style={{ marginBottom: marginBottomValue }}>
         <h3 style={{ marginBottom: '44px' }}>{data.titre}</h3>
         <div className='grid2' style={{ marginBottom: '8px' }}>
-          {data.Bloc_simple.map((b) => {
-            return (
-              <div key={b.id} className='bloc-simple'>
-                {b.page_cible ? (
-                  <Link href={b.page_cible} target={b.page_cible && b.page_cible.includes('https://') ? '_blank' : undefined}>
-                    <span className='blue-text'>{b.texte_en_valeur} </span>
-                    {b.texte}
-                  </Link>
-                ) : (
-                  <>
-                    <span className='blue-text'>{b.texte_en_valeur} </span>
-                    {b.texte}
-                  </>
-                )}
-              </div>
-            );
-          })}
+          {data.Bloc_simple.map((b) => (
+            <div key={b.id} className='bloc-simple'>
+              {b.page_cible ? (
+                <Link href={b.page_cible} target={b.page_cible && b.page_cible.includes('https://') ? '_blank' : undefined}>
+                  <span className='blue-text'>{b.texte_en_valeur} </span>
+                  {b.texte}
+                </Link>
+              ) : (
+                <>
+                  <span className='blue-text'>{b.texte_en_valeur} </span>
+                  {b.texte}
+                </>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
