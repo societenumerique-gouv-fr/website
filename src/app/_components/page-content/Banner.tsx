aff2335f4a10ba9c855da3fa4ea628e78baef82a
LAU-MG <90914885+LAU-MG@users.noreply.github.com>
2024-08-21 17:17:43 +0200

    Fix/banner (#11)
    * fix: restauring banner file
    
    * fix: image of homepage banner
    
    * fix: resolve build errors

--- src/app/_components/page-content/Banner.tsx
+++ src/app/_components/page-content/Banner.tsx
@@ -3,19 +3,6 @@
 import { useState, useEffect } from 'react';
 import { fetchData } from '@/functions/fetcher';
 
-type BannerData = {
-  titre: string;
-  taille_du_titre: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-  image: {
-    data?: {
-      attributes: {
-        url: string;
-      };
-    };
-  };
-  couleur_du_titre: keyof typeof colors | null;
-};
-
 type Colors = {
   [key: string]: string;
 };
@@ -35,14 +22,12 @@ export const Banner = () => {
 
   const loadData = async () => {
     try {
-      const response = await fetchData(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/banniere`);
-      const data = await response.json(); // Typage après récupération
-      const bannerData: BannerData = data.data.attributes;
-
-      setTitle(bannerData.titre);
-      setTitleSize(bannerData.taille_du_titre);
-      setImageUrl(bannerData.image.data?.attributes.url || '');
-      setTitleColor(bannerData.couleur_du_titre);
+      const resp = await fetchData(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/banniere`);
+      const { titre, taille_du_titre, image, couleur_du_titre } = resp.data.attributes;
+      setTitle(titre);
+      setTitleSize(taille_du_titre);
+      setImageUrl(image.data?.attributes.url || '');
+      setTitleColor(couleur_du_titre);
     } catch (e) {
       console.log('Erreur', e);
     }
@@ -53,34 +38,39 @@ export const Banner = () => {
   }, []);
 
   return (
-    <div className='banner' style={{ backgroundImage: `url(${imageUrl})`, marginBottom: '32px' }}>
+    <div
+      className='banner'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        marginBottom: '32px'
+      }}>
       {titleSize === 'h1' && (
-        <h1 className='banner-title' style={{ color: colors[titleColor!] }}>
+        <h1 className='banner-title' style={{ color: titleColor ? colors[titleColor] : undefined }}>
           {title}
         </h1>
       )}
       {titleSize === 'h2' && (
-        <h2 className='banner-title' style={{ color: colors[titleColor!] }}>
+        <h2 className='banner-title' style={{ color: titleColor ? colors[titleColor] : undefined }}>
           {title}
         </h2>
       )}
       {titleSize === 'h3' && (
-        <h3 className='banner-title' style={{ color: colors[titleColor!] }}>
+        <h3 className='banner-title' style={{ color: titleColor ? colors[titleColor] : undefined }}>
           {title}
         </h3>
       )}
       {titleSize === 'h4' && (
-        <h4 className='banner-title' style={{ color: colors[titleColor!] }}>
+        <h4 className='banner-title' style={{ color: titleColor ? colors[titleColor] : undefined }}>
           {title}
         </h4>
       )}
       {titleSize === 'h5' && (
-        <h5 className='banner-title' style={{ color: colors[titleColor!] }}>
+        <h5 className='banner-title' style={{ color: titleColor ? colors[titleColor] : undefined }}>
           {title}
         </h5>
       )}
       {titleSize === 'h6' && (
-        <h6 className='banner-title' style={{ color: colors[titleColor!] }}>
+        <h6 className='banner-title' style={{ color: titleColor ? colors[titleColor] : undefined }}>
           {title}
         </h6>
       )}
