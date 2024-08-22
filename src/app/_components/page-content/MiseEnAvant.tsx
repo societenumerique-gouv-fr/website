6c524783489889563480a9b02e8dc4d18471ddfa
Laura Miguel <laura.miguel.v2.4@gmail.com>
2024-08-22 15:25:01 +0200

    fix: restaured MiseEnAvant file

--- src/app/_components/page-content/MiseEnAvant.tsx
+++ src/app/_components/page-content/MiseEnAvant.tsx
@@ -1,18 +1,32 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
+'use client';
 
 import Link from 'next/link';
 import { marginsBottom } from '../structs';
 
-export const MiseEnAvant = ({ data }) => {
-  const giveMePath = (svg_coords) => {
+type MiseEnAvantProps = {
+  data: {
+    pictogramme_remixicon: string;
+    afficher_pictogramme: string;
+    espacement_bas: keyof typeof marginsBottom;
+    titre_du_bouton: string;
+    texte: string;
+    titre: string;
+    lien_du_bouton: string;
+  };
+};
+
+export const MiseEnAvant = ({ data }: MiseEnAvantProps) => {
+  const marginBottomValue = marginsBottom[data.espacement_bas] || '0px';
+
+  const giveMePath = (svg_coords: string) => {
     const start_path = svg_coords.indexOf('<path d="') + 9;
     const end_path = svg_coords.indexOf('"></path>');
     return svg_coords.slice(start_path, end_path);
   };
 
   return (
-    <div className={`fr-callout`} style={{ marginTop: '32px', marginBottom: marginsBottom[data.espacement_bas] }}>
+    <div className={`fr-callout`} style={{ marginTop: '32px', marginBottom: marginBottomValue }}>
+      {' '}
       {data.afficher_pictogramme && data.pictogramme_remixicon && (
         <div style={{ width: '25px', position: 'absolute', top: '16px', left: '16px' }}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
@@ -25,7 +39,7 @@ export const MiseEnAvant = ({ data }) => {
       {data.titre_du_bouton && (
         <Link
           href={data.lien_du_bouton}
-          target={data.lien_du_bouton && data.lien_du_bouton.includes('https://') ? '_blank' : undefined}
+          target={data.lien_du_bouton.includes('https://') ? '_blank' : undefined}
           className='fr-btn'>
           {data.titre_du_bouton}
         </Link>
