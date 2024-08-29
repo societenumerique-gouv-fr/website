b7313a374393e79147a609422d4de26d228169dd
LAU-MG <90914885+LAU-MG@users.noreply.github.com>
2024-08-29 15:37:06 +0200

    fix: restaured TextArea file (#23)
    * fix: restaured TextArea file
    
    * fix: restaured TextArea file

--- src/app/_components/page-content/TextArea.tsx
+++ src/app/_components/page-content/TextArea.tsx
@@ -1,36 +1,56 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import React from 'react';
 import Link from 'next/link';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown, { Components } from 'react-markdown';
 import { marginsBottom, position } from '../structs';
 
-export const TextArea = ({ data, row }) => {
-  const tabulations = {
-    Aucune: '0%',
-    Petite: '8%',
-    Moyenne: '16%',
-    Grande: '24%'
+type TextAreaProps = {
+  data: {
+    position: 'Gauche' | 'Centre' | 'Droite';
+    espacement_bas: keyof typeof marginsBottom;
+    tabulation: keyof typeof tabulations;
+    texte: string;
   };
+  row: number;
+};
 
-  const components = {
-    a: ({ ...props }) => (
-      <Link href={props.href} target={props.href.includes('https') ? '_blank' : undefined}>
-        {props.children}
+const tabulations: Record<'Aucune' | 'Petite' | 'Moyenne' | 'Grande', string> = {
+  Aucune: '0%',
+  Petite: '8%',
+  Moyenne: '16%',
+  Grande: '24%'
+};
+
+const components: Components = {
+  a: ({ href, children, ...props }) => {
+    if (!href) {
+      return <span {...props}>{children}</span>;
+    }
+
+    return (
+      <Link href={href} passHref>
+        <a target={href.includes('https') ? '_blank' : undefined} {...props}>
+          {children}
+        </a>
       </Link>
-    )
-  };
+    );
+  }
+};
+
+export const TextArea = ({ data, row }: TextAreaProps) => {
+  const positionClass = position[data.position as keyof typeof position] || '';
 
   return (
-    <div style={{ marginLeft: data.position == 'Centre' ? '10%' : null, marginBottom: marginsBottom[data.espacement_bas] }}>
+    <div
+      style={{
+        marginLeft: data.position === 'Centre' ? '10%' : undefined,
+        marginBottom: marginsBottom[data.espacement_bas] || '0px'
+      }}>
       <div
-        className={`textarea ${position[data.position] == 'left' ? '' : 'ml40'}`}
+        className={`textarea ${positionClass === 'left' ? '' : 'ml40'}`}
         style={{
-          marginLeft: row != null ? '0' : '',
-          wordBreak: 'break-word',
-          marginLeft: tabulations[data.tabulation],
-          marginRight: tabulations[data.tabulation]
+          marginLeft: row != null ? '0' : tabulations[data.tabulation] || '0%',
+          marginRight: tabulations[data.tabulation] || '0%',
+          wordBreak: 'break-word'
         }}>
         <ReactMarkdown components={components}>{data.texte}</ReactMarkdown>
       </div>
