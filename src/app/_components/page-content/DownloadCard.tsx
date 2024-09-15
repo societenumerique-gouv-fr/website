1b478c38c43aa5888e6fb944b9b9cb1ecdca7d4f
Marc Gavanier <marc.gavanier@gmail.com>
2024-09-15 22:51:27 +0200

    fix: remove type field from carte telecharegment

--- src/app/_components/page-content/DownloadCard.tsx
+++ src/app/_components/page-content/DownloadCard.tsx
@@ -46,123 +46,65 @@ export const DownloadCard = ({ data, rows }: DownloadCardProps) => {
   const colorClass = data.label?.couleur_du_label ? labelColors[data.label.couleur_du_label] : 'defaultColorClass';
 
   return (
-    <>
-      {data.type_de_carte === 'Tuile' && (
-        <div
-          key={data.id}
-          style={
-            rows === 1
-              ? {
-                  display: 'flex',
-                  justifyContent: data.position === 'Centre' ? 'center' : data.position === 'Gauche' ? 'flex-start' : 'flex-end'
-                }
-              : {}
-          }>
-          <div
-            className='fr-tile fr-tile--download fr-enlarge-link mb2'
-            id='tile-6735'
-            style={{ marginBottom: marginBottomValue }}>
-            <div className='fr-tile__body'>
-              <div className='fr-tile__content'>
-                <h3 className='fr-tile__title'>
-                  <a
-                    href={
-                      data.media_a_telecharger.data
-                        ? data.media_a_telecharger.data.attributes.url
-                        : data.telechargement_externe || '#'
-                    }
-                    download
-                    target='_blank'
-                    data-fr-assess-file='arraybuffer'
-                    hrefLang='fr'>
-                    {data.titre_de_la_carte}
-                  </a>
-                </h3>
-                <p className='fr-card__desc'>
-                  {data.label && (
-                    <span
-                      className={`card-label ${colorClass}`}
-                      style={{ fontSize: '14px', display: 'block', marginTop: '0px', marginBottom: '16px' }}>
-                      {data.label.titre_du_label}
-                    </span>
-                  )}
-                  {data.description_de_la_carte}
-                </p>
-                <p className='fr-tile__detail'>Détail (optionel)</p>
-              </div>
+    <div
+      className={[
+        rows === 1
+          ? `fr-grid-row fr-grid-row--gutters ${marginsBottom[data.espacement_bas]} fr-col-md-6`
+          : `fr-col-md-${12 / rows} fr-col ${marginsBottom[data.espacement_bas]}}`,
+        data.position === 'Centre' ? 'fr-mx-auto' : undefined,
+        data.position === 'Gauche' ? 'fr-mr-auto' : undefined,
+        data.position === 'Droite' ? 'fr-ms-auto' : undefined
+      ].join(' ')}
+      key={data.id}>
+      <div
+        className='fr-card fr-enlarge-link fr-card--horizontal fr-card--horizontal-half mb2'
+        style={{ marginBottom: marginBottomValue }}>
+        <div className='fr-card__body'>
+          <div className='fr-card__content'>
+            <h3 className='fr-card__title blue-text'>{data.titre_de_la_carte}</h3>
+            <a
+              href={
+                data.media_a_telecharger.data
+                  ? data.media_a_telecharger.data.attributes.url
+                  : data.telechargement_externe || '#'
+              }
+              target='_blank'
+              hrefLang='fr'></a>
+            <p className='fr-card__desc'>{data.description_de_la_carte}</p>
+            <div className='fr-card__start'>
+              {data.label && (
+                <span
+                  className={`card-label ${colorClass}`}
+                  style={{ fontSize: '14px', display: 'block', marginTop: '0px', marginBottom: '16px' }}>
+                  {data.label.titre_du_label}
+                </span>
+              )}
             </div>
-            {data.image_de_la_carte.data?.attributes.url && (
-              <div className='fr-tile__header'>
-                <div className='fr-tile__pictogram'>
-                  <img src={data.image_de_la_carte.data.attributes.url} alt='' />
-                </div>
-              </div>
-            )}
+            <div className='fr-card__end'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                style={{ width: '25px', position: 'absolute', right: '24px', bottom: '24px' }}
+                viewBox='0 0 24 24'>
+                <path
+                  d='M3 19H21V21H3V19ZM13 13.1716L19.0711 7.1005L20.4853 8.51472L12 17L3.51472 8.51472L4.92893 7.1005L11 13.1716V2H13V13.1716Z'
+                  fill='#000091'></path>
+              </svg>
+            </div>
           </div>
         </div>
-      )}
-      {data.type_de_carte === 'Classique' && (
-        <div
-          className={[
-            rows === 1
-              ? `fr-grid-row fr-grid-row--gutters ${marginsBottom[data.espacement_bas]} fr-col-md-6`
-              : `fr-col-md-${12 / rows} fr-col ${marginsBottom[data.espacement_bas]}}`,
-            data.position === 'Centre' ? 'fr-mx-auto' : undefined,
-            data.position === 'Gauche' ? 'fr-mr-auto' : undefined,
-            data.position === 'Droite' ? 'fr-ms-auto' : undefined
-          ].join(' ')}
-          key={data.id}>
-          <div
-            className='fr-card fr-enlarge-link fr-card--horizontal fr-card--horizontal-half mb2'
-            style={{ marginBottom: marginBottomValue }}>
-            <div className='fr-card__body'>
-              <div className='fr-card__content'>
-                <h3 className='fr-card__title blue-text'>{data.titre_de_la_carte}</h3>
-                <a
-                  href={
-                    data.media_a_telecharger.data
-                      ? data.media_a_telecharger.data.attributes.url
-                      : data.telechargement_externe || '#'
-                  }
-                  target='_blank'
-                  hrefLang='fr'></a>
-                <p className='fr-card__desc'>{data.description_de_la_carte}</p>
-                <div className='fr-card__start'>
-                  {data.label && (
-                    <span
-                      className={`card-label ${colorClass}`}
-                      style={{ fontSize: '14px', display: 'block', marginTop: '0px', marginBottom: '16px' }}>
-                      {data.label.titre_du_label}
-                    </span>
-                  )}
-                </div>
-                <div className='fr-card__end'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    style={{ width: '25px', position: 'absolute', right: '24px', bottom: '24px' }}
-                    viewBox='0 0 24 24'>
-                    <path
-                      d='M3 19H21V21H3V19ZM13 13.1716L19.0711 7.1005L20.4853 8.51472L12 17L3.51472 8.51472L4.92893 7.1005L11 13.1716V2H13V13.1716Z'
-                      fill='#000091'></path>
-                  </svg>
-                </div>
-              </div>
+        {data.image_de_la_carte.data?.attributes.url && (
+          <div className='fr-card__header fr-col-md-4'>
+            <div className='fr-card__img'>
+              <img
+                className='fr-p-3w'
+                style={{ width: '100%', objectFit: 'contain' }}
+                src={data.image_de_la_carte.data.attributes.url}
+                alt=''
+              />
             </div>
-            {data.image_de_la_carte.data?.attributes.url && (
-              <div className='fr-card__header fr-col-md-4'>
-                <div className='fr-card__img'>
-                  <img
-                    className='fr-p-3w'
-                    style={{ width: '100%', objectFit: 'contain' }}
-                    src={data.image_de_la_carte.data.attributes.url}
-                    alt=''
-                  />
-                </div>
-              </div>
-            )}
           </div>
-        </div>
-      )}
-    </>
+        )}
+      </div>
+    </div>
   );
 };
